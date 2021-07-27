7f7856c9f14ee4ad453c2aad1fb9f547c13c321f
Ren0503 <illuminati0440@gmail.com>
2021-07-27 20:16:41 +0700

    Edit router

---
+++ server/src/routes/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+const TagCtrl = require('../controllers/tagControllers');
+const UserCtrl = require('../controllers/userControllers');
+const VoteCtrl = require('../controllers/voteControllers');
+const AnswerCtrl = require('../controllers/answerControllers');
+const CommentCtrl = require('../controllers/commentControllers');
+const QuestionCtrl = require('../controllers/questionControllers');
+
+const {
+    protect,
+    answerAuth,
+    commentAuth,
+    questionAuth,
+} = require('../middleware/authMiddleware');
+
+//  Authentication
+router.post('/signup', UserCtrl.validateUser, UserCtrl.signup);
+router.post('/authenticate', UserCtrl.validateUser, UserCtrl.authenticate);
+
+//  Users
+router.get('/users', UserCtrl.listUsers);
+router.get('/users/:search', UserCtrl.search);
+router.get('/users/:username', UserCtrl.find);
+
+//  Questions
+router.param('question', QuestionCtrl.loadQuestions);
+router.post('/questions', [protect, QuestionCtrl.questionValidate], QuestionCtrl.createQuestion);
+router.get('/question/:question', QuestionCtrl.show);
+router.get('/question', QuestionCtrl.listQuestions);
+router.get('/question/:tags', QuestionCtrl.listByTags);
+router.get('/question/user/:username', QuestionCtrl.listByUser);
+router.delete('/question/:question', [protect, questionAuth], QuestionCtrl.removeQuestion);
+
+//  Tags
+router.get('/tags/popularTags', TagCtrl.listPopularTags);
+router.get('/tags/:tag', TagCtrl.searchTags);
+router.get('/tags', TagCtrl.listTags);
+
+//  Answers
+router.param('answer', AnswerCtrl.loadAnswers);
+router.post('/answer/:question', [protect, AnswerCtrl.answerValidate], AnswerCtrl.createAnswer);
+router.delete('/answer/:question/:answer', [protect, answerAuth], AnswerCtrl.removeAnswer);
+
+//  Votes
+router.get('/votes/upvote/:question/:answer?', protect, VoteCtrl.upvote);
+router.get('/votes/downvote/:question/:answer?', protect, VoteCtrl.downvote);
+router.get('/votes/unvote/:question/:answer?', protect, VoteCtrl.unvote);
+
+//  Comments
+router.param('comment', CommentCtrl.loadComments);
+router.post('/comment/:question/:answer?', [protect, CommentCtrl.validate], CommentCtrl.createComment);
+router.delete('/comment/:question/:comment', [protect, commentAuth], CommentCtrl.removeComment);
+router.delete('/comment/:question/:answer/:comment', [protect, commentAuth], CommentCtrl.removeComment);
+
+module.exports = router;
--- server/src/server.js
+++ server/src/server.js
@@ -5,13 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const voteRoutes = require('./routes/voteRoutes');
-const commentRoutes = require('./routes/commentRoutes');
-const tagRoutes = require('./routes/tagRoutes');
-const answerRoutes = require('./routes/answerRoutes');
-const questionRoutes = require('./routes/questionRoutes');
+const router = require('./routes');
 
 connectDB();
 
@@ -22,13 +16,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/question', questionRoutes);
-app.use('/api/answer', answerRoutes);
-app.use('/api/votes', voteRoutes);
-app.use('/api/comment', commentRoutes);
-app.use('/api/tags', tagRoutes);
+app.use('/api', router);
 
 const PORT = process.env.PORT || 5000;
 
